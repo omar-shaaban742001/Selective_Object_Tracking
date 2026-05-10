94d7bd087545bbc90cbac196cc8683c27b5d3e28
omar-shaaban742001 <omarshaaban742001@gmail.com>
2026-05-10 22:00:41 +0300

    done detecor on images

--- detection/detector.py
+++ detection/detector.py
@@ -1,12 +1,32 @@
+"""
+Run Yolo model on frame to get detection 
+"""
+
+
 from ultralytics import YOLO
 
 def detector(frame, model_path):
 
     model = YOLO(model_path)
+    model_names = model.names
+    results = model(frame)[0]
 
+    detections = []
 
-    results = model(frame, save=True)
+    for box in results.boxes:
 
-    return results
+        x1, y1, x2, y2 = box.xyxy[0].tolist()
+
+        conf = float(box.conf[0])
+
+        cls = int(box.cls[0])
+
+        detections.append({
+            "bbox": [x1, y1, x2, y2],
+            "confidence": conf,
+            "class_id": cls
+        })
+
+    return detections, model_names
     
     
--- main.py
+++ main.py
@@ -1,8 +1,55 @@
 from ultralytics import YOLO
 from configs import config
 from detection import detector
+import cv2
 
-output = detector(r"E:\Selective_Object_Tracking\WhatsApp Image 2026-05-07 at 15.53.54.jpeg",
-                  config.MODEL_PATH)
+cap = cv2.VideoCapture(r"E:\Selective_Object_Tracking\outputs\Drone Street Traffic, New York City.mp4")
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps = int(cap.get(cv2.CAP_PROP_FPS))
 
-print(output)
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+
+writer = cv2.VideoWriter(
+    "outputs/output.mp4",
+    fourcc,
+    fps,
+    (width, height)
+)
+
+while True:
+    ret, frame = cap.read()
+    
+    if not ret:
+        break
+    
+
+    detections, names = detector(frame,
+                    config.MODEL_PATH)
+    
+
+    for det in detections:
+
+        if names[det['class_id']] == 'car':
+            x1, y1, x2, y2 = map(int, det["bbox"])
+    
+            cv2.rectangle(
+                frame,
+                (x1, y1),
+                (x2, y2),
+                (0,255,0),
+                2
+            )
+
+
+    writer.write(frame)
+
+    cv2.imshow("Detection", frame)
+
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+
+cap.release()
+writer.release()
+cv2.destroyAllWindows()
